4c307f78e73760e8e47698c5161736962aac1fba
Mariia Kovalchuk <mariiakovalchuk.ua@gmail.com>
2021-08-03 01:39:01 +0300

    hooks update

--- src/ImageGallery/ImageGallery.js
+++ src/ImageGallery/ImageGallery.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState, useEffect} from 'react';
 import styles from './ImageGallery.module.css';
 import Searchbar from '../Searchbar';
 import apiSearchService from '../apiSearchService';
@@ -15,53 +15,47 @@ const Status = {
   REJECTED: 'rejected',
 };
 
-function ImageGallery(params) {
+function ImageGallery() {
     
     const [searchQuery, setSearchQuery] = useState('');
     const [page, setPage] = useState(1);
     const [status, setStatus] = useState(Status.IDLE);
     const [searchQueryResult, setSearchQueryResult] = useState([]);
     const [showModal, setShowModal] = useState(false);
-//   const [currentImage, setCurrentImage] = useState('');
     const [modalImgUrl, setModalImgUrl] = useState(null);
     const [total, setTotal] = useState(null);
     const [error, setError] = useState(null);
 
 
-    // listRef = React.createRef();
-    const listRef = useRef(null);
 
-    // const scrollToRef = (ref) => window.scrollTo({ top: ref.current.scrollHeight, behavior: 'smooth' });
-    // const scrollToTop = () => scrollToRef(listRef);
-
-    // getSnapshotBeforeUpdate(prevProps, prevState) {
-    // // Are we adding new items to the list?
-    // // Capture the scroll position so we can adjust scroll later.
-    // if (prevState.searchQueryResult.length < this.state.searchQueryResult.length) {
-    //     const list = this.listRef.current;
-    //     return list.scrollHeight - list.scrollTop;
-    //       };
-    //       return null;
-    // };
     useEffect(() => {
         if (!searchQuery) {
             return;
+        };
+        
+        const fetchSearchQuery = () => {
+            apiSearchService
+                .fetchSearchQuery(searchQuery, page)
+                .then(searchQueryResult => {
+                    setSearchQueryResult(prevState => [...prevState, ...searchQueryResult.hits]);
+                    setStatus(Status.RESOLVED);
+                    setTotal(searchQueryResult.total);
+                })
+                .catch(error => {
+                    setError(error);
+                    setStatus(Status.REJECTED);
+                })
+                .finally(() => window.scrollTo({
+                    top: document.documentElement.scrollHeight,
+                    behavior: "smooth",
+                }));
+            
         };
 
         fetchSearchQuery();
         setStatus(Status.PENDING);
 
-        // if (snapshot !== null) {
-        //     window.scrollTo({
-        //         top: snapshot,
-        //         behavior: 'smooth',
-        //     });
-        // };
-
-        // return () => {
-        //     cleanup
-        // }
-    }, [searchQuery]);
+    }, [searchQuery,page]);    
 
 
 
@@ -69,23 +63,7 @@ function ImageGallery(params) {
         setSearchQuery(searchQuery);
         setPage(1);
         setSearchQueryResult([]);
-    };
-
-    const fetchSearchQuery = () => {
-        apiSearchService
-            .fetchSearchQuery(searchQuery, page)
-            .then(searchQueryResult => {
-                setSearchQueryResult(prevState => [...prevState, ...searchQueryResult.hits]);
-                setStatus(Status.RESOLVED);
-                setPage(prevPage => prevPage + 1);
-                setTotal(searchQueryResult.total);
-            })
-            .catch(error => {
-                setError(error);
-                setStatus(Status.REJECTED);
-            })
-    };
-
+    };    
 
     const toggleModal = () => {
         setShowModal(!showModal);
@@ -104,6 +82,9 @@ function ImageGallery(params) {
           
     };
     
+      const onLoadMore = () => {
+          setPage((prevState) => prevState + 1);
+  };
     
     return (
             <div >
@@ -111,7 +92,7 @@ function ImageGallery(params) {
 
                 {status === 'pending' && <Loader className={styles.Loader} type="Circles" color="#3f51b5" height={100} width={100} timeout={5000} />}
 
-                <div ref={listRef}>
+                <div >
                     {status === 'resolved' && searchQueryResult.length > 0 &&
                         <div >
                             <ul className={styles.ImageGallery}>
@@ -120,7 +101,7 @@ function ImageGallery(params) {
                                 ))}
                             </ul>
 
-                            {total > (page - 1) * 12 && (<Button onClick={fetchSearchQuery} />)}
+                            {total > (page - 1) * 12 && (<Button onClick={onLoadMore} />)}
                
                             {showModal && <Modal onClose={toggleModal} url={modalImgUrl} />}
 
@@ -137,126 +118,8 @@ function ImageGallery(params) {
             
         );
         
-
-
-
 };
 
 
-// class ImageGallery extends Component{
-
-//     state = {
-//         searchQuery: '',
-//         page: 1,
-//         status: Status.IDLE,
-//         searchQueryResult: [],
-//         showModal: false,
-//         modalImgUrl: null,
-//         total: null,
-//         error: null
-
-//     }
-//     listRef = React.createRef()
-
-//     getSnapshotBeforeUpdate(prevProps, prevState) {
-//     // Are we adding new items to the list?
-//     // Capture the scroll position so we can adjust scroll later.
-//     if (prevState.searchQueryResult.length < this.state.searchQueryResult.length) {
-//         const list = this.listRef.current;
-//         return list.scrollHeight - list.scrollTop;
-//           };
-//           return null;
-//     };
-
-//     componentDidUpdate(prevProps, prevState, snapshot) {
-//         const prevSearchQuery = prevState.searchQuery;
-//         const nextSearchQuery = this.state.searchQuery;
-
-//         if (prevSearchQuery !== nextSearchQuery) {
-//             this.setState({ status: Status.PENDING});
-//             this.fetchSearchQuery()
-//         };
-
-//         if (snapshot !== null) {
-//             window.scrollTo({
-//         top: snapshot,
-//         behavior: 'smooth',
-//       });
-//         };
-//     };
-
-//     handleFormSubmit = searchQuery => {
-//         this.setState({ searchQuery, page:1, searchQueryResult: [] });
-//     };
-
-//     fetchSearchQuery = () => {
-//         const { searchQuery, page } = this.state;
-//         apiSearchService
-//             .fetchSearchQuery(searchQuery, page)
-//             .then(searchQueryResult => {
-//                 this.setState(prevState => ({ searchQueryResult: [...prevState.searchQueryResult, ...searchQueryResult.hits], status: Status.RESOLVED, page: prevState.page+1, total: searchQueryResult.total }))
-//             })
-//             .catch(error => this.setState({ error, status: Status.REJECTED }))
-//     };
-
-
-//     toggleModal = () => {
-//         this.setState(({ showModal }) => ({
-//             showModal: !showModal,
-//         }));
-//     };
-    
-//     handleImgURL = modalImgUrl => {
-//         this.setState({ modalImgUrl });
-//     };
-
-//     handleImgClick = event => {
-//         if (event.currentTarget === event.target) {
-//             const largeImageURL = event.target.dataset.source;
-//             this.handleImgURL(largeImageURL)
-//             this.toggleModal()
-//         }
-          
-//     };
-
-
-
-//     render() {
-//         const { searchQueryResult, status, error, modalImgUrl, showModal, total, page } = this.state;
-
-//         return (
-//             <div >
-//                 < Searchbar onSubmit={this.handleFormSubmit} />
-
-//                 {status === 'pending' && <Loader className={styles.Loader} type="Circles" color="#3f51b5" height={100} width={100} timeout={5000} />}
-
-//                 <div ref={this.listRef}>
-//                     {status === 'resolved' && searchQueryResult.length > 0 &&
-//                         <div >
-//                             <ul className={styles.ImageGallery}>
-//                                 {searchQueryResult.map(img => (
-//                                     <ImageGalleryItem onClick={this.handleImgClick} key={img.id} img={img} />
-//                                 ))}
-//                             </ul>
-
-//                             {total > (page - 1) * 12 && (<Button onClick={this.fetchSearchQuery} />)}
-               
-//                             {showModal && <Modal onClose={this.toggleModal} url={modalImgUrl} />}
-
-//                         </div>}
-//                 </div>
-                
-//                 {status === 'resolved' && !searchQueryResult.length &&
-//                     <p className={styles.ErrorQuery}>No match found. Please check your query!</p>}
-                
-//                 {status === 'rejected' && <p className={styles.ErrorQuery}>{error.message}</p>}
-                           
-
-//             </div>
-            
-//         );
-        
-//     };
-// }
 
 export default ImageGallery;
--- src/Searchbar/Searchbar.js
+++ src/Searchbar/Searchbar.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import styles from './Searchbar.module.css';
 import { error } from "@pnotify/core";
 import "@pnotify/core/dist/PNotify.css";
@@ -7,51 +7,47 @@ import PropTypes from 'prop-types';
 
 
 
-class Searchbar extends Component{
-    state = {
-        searchQuery: ''
+function Searchbar({onSubmit}) {
+    
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const handleSearchQueryChange = event => {
+        setSearchQuery(event.currentTarget.value.toLowerCase())
+    };
+
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        if (searchQuery.trim() === '') {
+            error({
+                text: "Please enter your search query!",
+                delay: 2000,
+            });
+            return
+        };
+
+        onSubmit(searchQuery);
+        setSearchQuery('')
         
     };
 
-    handleSearchQueryChange = event => {
-        this.setState({ searchQuery: event.currentTarget.value.toLowerCase()})
-    }
+    return (
+        <header className={styles.Searchbar}>
+            <form className={styles.SearchForm} onSubmit={handleSubmit}>
+                <button type="submit" className={styles.SearchFormButton}>
+                    <span className={styles.SearchFormButtonLabel}>Search</span>
+                </button>
 
-    handleSubmit = event => {
-        event.preventDefault();
-        if (this.state.searchQuery.trim() === '') {
-            error({
-                  text: "Please enter your search query!",
-                  delay: 2000,
-              });
-              return
-        };
-
-    this.props.onSubmit(this.state.searchQuery);
-    this.setState({ searchQuery: '' });
-
-
-    }
-
-    render() {
-        return (
-            <header className={styles.Searchbar}>
-                <form className={styles.SearchForm} onSubmit={this.handleSubmit}>
-                    <button type="submit" className={styles.SearchFormButton}>
-                        <span className={styles.SearchFormButtonLabel}>Search</span>
-                    </button>
-
-                    <input
-                        className={styles.SearchFormInput}
-                        type="text"
-                        placeholder="Search images and photos"
-                        onChange={this.handleSearchQueryChange}
-                        value={this.state.searchQuery}
-                    />
-                </form>
-            </header>
-        );
-    }
+                <input
+                    className={styles.SearchFormInput}
+                    type="text"
+                    placeholder="Search images and photos"
+                    onChange={handleSearchQueryChange}
+                    value={searchQuery}
+                />
+            </form>
+        </header>
+    );
 }
 
 export default Searchbar;
